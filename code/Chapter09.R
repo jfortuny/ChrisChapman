37505a6c655fb60d79e8bb3b458993bc8da1c8e1
jfortuny <jfortuny@jfortuny.com>
2016-10-27 15:52:37 -0400

    more chapter 9

--- code/Chapter09.R
+++ code/Chapter09.R
@@ -1,3 +1,13 @@
 # 9.1.1 #######################################################################
 cust.df <- read.csv("http://goo.gl/PmPkaG")
 summary(cust.df)
+
+# blind approach
+spend.m1 <- lm(online.spend ~ ., data = subset(cust.df[,-1], online.spend > 0))
+summary(spend.m1)
+
+library(gpairs)
+gpairs(cust.df[,-1])
+
+library(car)
+scatterplotMatrix(cust.df[,-1], diagonal = "histogram")
