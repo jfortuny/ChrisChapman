9da86a11db7d950126d628893904439a4975e3e1
jfortuny <jfortuny@jfortuny.com>
2016-10-29 06:54:26 -0400

    multi collinearity start

--- code/Chapter09.R
+++ code/Chapter09.R
@@ -11,3 +11,15 @@ gpairs(cust.df[,-1])
 
 library(car)
 scatterplotMatrix(cust.df[,-1], diagonal = "histogram")
+
+# auto transform variables
+autoTransform <- function(x) {
+  library(forecast)
+  return(scale(BoxCox(x, BoxCox.lambda(x))))
+}
+# select complete cases and drop customer Id
+cust.df.bc <- cust.df[complete.cases(cust.df), -1]
+# select cases with online spend
+cust.df.bc <- subset(cust.df.bc, online.spend > 0)
+# create a vector to index columns except email
+numcols <- which(colnames(cust.df.bc) != "email")
