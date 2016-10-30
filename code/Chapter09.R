8a39b64154b537b2d1eadf5f1f3bd252367cdd93
jfortuny <jfortuny@jfortuny.com>
2016-10-30 06:50:05 -0400

    completed section 9.1

--- code/Chapter09.R
+++ code/Chapter09.R
@@ -23,3 +23,33 @@ cust.df.bc <- cust.df[complete.cases(cust.df), -1]
 cust.df.bc <- subset(cust.df.bc, online.spend > 0)
 # create a vector to index columns except email
 numcols <- which(colnames(cust.df.bc) != "email")
+# apply the transformation to all columns
+cust.df.bc[, numcols] <- lapply(cust.df.bc[, numcols], autoTransform)
+summary(cust.df.bc)
+
+library(car)
+scatterplotMatrix(cust.df.bc, diagonal = "histogram")
+
+spend.m2 <- lm(online.spend ~ ., data = cust.df.bc)
+summary(spend.m2)
+
+# 9.1.2 #######################################################################
+library(car)
+vif(spend.m2)
+
+# eliminate highly correlated variables
+spend.m4 <- lm(online.spend ~ . -online.trans -store.trans, data = cust.df.bc)
+summary(spend.m4)
+vif(spend.m4)
+
+# use PCA to replace correlated variables (use first PC)
+pc.online <- prcomp(cust.df.bc[, c("online.visits", "online.trans")])
+cust.df.bc$online <- pc.online$x[, 1]
+pc.store <- prcomp((cust.df.bc[, c("store.trans", "store.spend")]))
+cust.df.bc$store <- pc.store$x[, 1]
+head(cust.df.bc)
+spend.m5 <- lm(online.spend ~ email + age + credit.score + distance.to.store +
+                 sat.service + sat.selection + online + store,
+               data =  cust.df.bc)
+summary(spend.m5)
+vif(spend.m5)
