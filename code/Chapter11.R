22aee83d9e3c569818e567439d40d09893e7afa8
jfortuny <jfortuny@jfortuny.com>
2016-11-29 15:43:23 -0500

    completed chapter 11

--- code/Chapter11.R
+++ code/Chapter11.R
@@ -1,7 +1,7 @@
 # 11.2 ########################################################################
 #
-load("~/Rwork/ChrisChapman/data/Chapter05.RData")
-#load("U:/My Documents/R Projects/ChrisChapman/data/Chapter05.RData")
+#load("~/Rwork/ChrisChapman/data/Chapter05.RData")
+load("U:/My Documents/R Projects/ChrisChapman/data/Chapter05.RData")
 seg.raw <- seg.df
 seg.df <- seg.raw[, -7]
 
@@ -82,3 +82,61 @@ library(cluster)
 clusplot(seg.df.test[,-7], seg.nb.class,
          color = TRUE, shade = TRUE, labels = 4, lines = 0,
          main = "Nasive Bayes classification, holdout data")
+
+# 11.4.2 ######################################################################
+#
+set.seed(04625)
+train.prop <- 0.65
+train.cases <- sample(nrow(seg.raw), nrow(seg.raw) * train.prop)
+seg.df.train <- seg.raw[train.cases,]
+seg.df.test <- seg.raw[-train.cases,]
+
+library(randomForest)
+set.seed(98040)
+(seg.rf <- randomForest(formula = Segment ~ ., data = seg.df.train, ntree = 3000))
+
+library(cluster)
+(seg.rf.class <- predict(seg.rf, seg.df.test))
+clusplot(seg.df.test[,-7], seg.rf.class,
+         color = TRUE, shade = TRUE, labels = 4, lines = 0,
+         main = "Random Forest classification, holdout data")
+
+mean(seg.df.test$Segment==seg.rf.class)
+table(seg.df.test$Segment, seg.rf.class)
+
+library(mclust)
+adjustedRandIndex(seg.df.test$Segment, seg.rf.class)
+
+# 11.4.3 ######################################################################
+#
+set.seed(98040)
+(seg.rf <- randomForest(formula = Segment ~ ., data = seg.df.train, ntree = 3000,
+                        importance = TRUE))
+importance(seg.rf)
+varImpPlot(seg.rf, main = "Variable importance by segment")
+
+# 11.5 ########################################################################
+#
+set.seed(92118)
+train.prop <- 0.65
+train.cases <- sample(nrow(seg.df), nrow(seg.df) * train.prop)
+sub.df.train <- seg.df[train.cases,]
+sub.df.test <- seg.df[-train.cases,]
+
+clusplot(sub.df.train[, -6], sub.df.train$subscribe,
+          color = TRUE, shade = TRUE,
+          labels = 4, lines = 0, main = "Subscribers clusters, training data")
+
+library(randomForest)
+set.seed(11954)
+(sub.rf <- randomForest(formula = subscribe ~ ., data = sub.df.train, ntree = 3000))
+
+# to correct the class imbalance we sample more from the small group
+set.seed(11954)
+(sub.rf <- randomForest(formula = subscribe ~ ., data = sub.df.train,
+                        ntree = 3000, sampsize = c(25,25)))
+
+sub.rf.sub <- predict(sub.rf, sub.df.test)
+table(sub.rf.sub, sub.df.test$subscribe)
+
+adjustedRandIndex(sub.rf.sub, sub.df.test$subscribe)
